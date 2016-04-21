6fd10af1b5d303d8d93147a2c0100c39dd5f967f
vmeng <verymeng@gmail.com>
2016-04-21 15:54:31 +0800

    friend

--- public/js/personalPage.js
+++ public/js/personalPage.js
@@ -1,11 +1,14 @@
 $(function() {
-	var tabValue = window.location.hash;
-	console.log(tabValue);
+    var tabValue = window.location.hash;
 
-	if(tabValue) {
-		 $('#myTabs a[href="#tabValue"]').tab('show'); // Select tab by name
-		 $("#pane2").append('<li><a href="/p2p-chat"><span class="tab">'+name+'</span></a></li>');
-	}
+    if (tabValue) {
 
+        var split = tabValue.split('?');
+        var pannel = split[0];
+        var name = split[1].split('=')[1];
+
+        $('#myTabs a[href="' + pannel + '"]').tab('show'); // Select tab by name
+        $("#pane2").append('<li><a href="/p2p-chat"><span class="tab">' + name + '</span></a></li>');
+    }
 
 });
--- routes/search.js
+++ routes/search.js
@@ -12,9 +12,12 @@ module.exports = function(app) {
         var userName = new RegExp(req.body.userName, 'i');
 
         UserItem.find({name: userName},function(err, users) {
-
+            var currentName = req.cookies.username;
+            users = users.filter(function(u) {
+                return currentName !== u.name;
+            });
             res.status(200).json( users);
-        })
+        });
         // Render views/home.ejs
 
     });
